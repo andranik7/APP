ba871beb2fcb0e8fe144b35fab7b452d74d3d361
Andranik <andranikhayra@gmail.com>
2019-04-17 22:23:55 +0200

    tableau client et graphique

---
+++ Vues/client.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil Utilisateur</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="Vues/css/alice.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+</head>
+<body class="bodyProfile">
+	<h2 style="color:#354a6d; font-weight: bold;">Profile</h2>
+	<div style="text-align:center">
+		<table class="rectangleMenuPanneau">
+			<tbody>
+				<tr>
+					<td>
+						<button class="btn-menu" id="1" onclick="changeStep(1)">
+							Gestion de l'habitat
+						</button>
+					</td>
+					<td>
+						<button class="btn-menu" id="2" onclick="changeStep(2)">
+							Relevé de consommation
+						</button>
+					</td>
+					<td>
+						<button class="btn-menu" id="3" onclick="changeStep(3)">
+							Contrôle des actionneurs
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+
+	<div class="rectangleContenu" id="rectangleContenu1">
+		<div class="title">
+			Gestion de mon habitat
+		</div>
+
+		<div class="block">
+			<div style="flex:1;" >
+				<table class="tableGestion">
+					<tr>
+						<th>Liste des habitats</th>
+					</tr>
+					<form action="" method="post">
+						<?php
+							for($i=0;$i<sizeof($listeLogements);$i++){
+								echo '<tr><td><input type="submit" name="logements" class="btn-tab" value="'.$listeLogements[$i]['adresse'].'"></td></tr>';
+							}
+							if(sizeof($listeLogements)==0)
+								echo "<tr><td>Vous n'avez pas encore de logements enregistrés. Ajoutez en un avec le bouton ci dessous</tr></td>";
+						?>
+					</form>
+
+				</table>
+
+				<button class="btn-ajouter" onclick="ajouterHabitat()" style="height: auto;">
+					Ajouter un habitat
+				</button>
+
+				<div id="newHabitat" style="display:none">
+					<form action="" method="post">
+						<div class="content">
+							<div style="flex: 1;">
+								Adresse
+							</div>
+							<div style="flex: 2">
+								<input type="text" required name="adresse" class="input_content" placeholder="Ex: 9 avenue Victor Hugo" style="border-radius:5px;">
+							</div>
+						</div>
+						<div class="content">
+							<div style="flex: 1;">
+								Ville
+							</div>
+							<div style="flex: 2">
+								<input type="text" required name="ville" class="input_content" placeholder="Ex: Paris" style="border-radius:5px;">
+							</div>
+						</div>
+						<div class="content">
+							<div style="flex: 1;">
+								Code postal
+							</div>
+							<div style="flex: 2">
+								<input type="text" required name="cp" class="input_content" placeholder="Ex: 92140" style="border-radius:5px;">
+							</div>
+						</div>
+						<div class="content">
+							<div style="flex: 1;">
+								Nombre d'habitants
+							</div>
+							<div style="flex: 2">
+								<input type="number" required name="nb_habitants" class="input_content" placeholder="Ex: 4" style="border-radius:5px;">
+							</div>
+						</div>
+						<div class="content">
+							<div style="flex: 1;">
+								Superficie (m2)
+							</div>
+							<div style="flex: 2">
+								<input type="number" required name="superficie" class="input_content" placeholder="Ex: 105" style="border-radius:5px;">
+							</div>
+						</div>
+						<button type="submit" class="btn-ajouter">Confirmer</button>
+					</form>
+
+
+				</div>
+
+			</div>
+
+			<div style="flex:1">
+
+				<div id="listePiece" >
+
+
+					<table class="tableGestion">
+						<tr>
+							<th>Liste des pieces</th>
+						</tr>
+						<form >
+							<?php
+							if(isset($listePieces)){
+								for($i=0;$i<sizeof($listePieces);$i++){
+									echo '<tr><td><input type="submit"  class="btn-tab" value="'.$listePieces[$i]['descriptif'].'"></td></tr>';
+								}
+								if(sizeof($listePieces)==0)
+									echo "<tr><td>Vous n'avez pas encore ajouté de pièces. Ajoutez en une avec le bouton ci dessous</tr></td>";
+							}else{
+								echo "<tr><td>Pour afficher les pièce d'un logement, veuillz en sélectionner un grâce au menu de gauche.</tr></td>";
+							}
+
+							?>
+						</form>
+					</table>
+
+
+					<button class="btn-ajouter" onclick="ajouterPiece()">
+						Ajouter une piece
+					</button>
+
+
+					<div id="newCapteur" style="display:none">
+						<form action="" method="post">
+							<div class="content">
+								<div style="flex: 1;">
+									Type de piece
+								</div>
+								<div style="flex: 2">
+									<input type="text" required name="typePiece" class="input_content" placeholder="Ex: Sallon, Chambre 1 ..." style="border-radius:5px;">
+								</div>
+							</div>
+							<div class="content">
+								<div style="flex: 1;">
+									Superficie (m2)
+								</div>
+								<div style="flex: 2">
+									<input type="number" required name="superficie" class="input_content" placeholder="Ex: 20" style="border-radius:5px;">
+								</div>
+							</div>
+							<button type="submit" class="btn-ajouter">Confirmer</button>
+						</form>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+
+		<div class="rectangleContenu" id="rectangleContenu2">
+			<div class="title">
+				Mon relevé de consommation
+			</div>
+			<div style="display:flex; margin-top:100px">
+				<div style="flex:10">
+					<table class="tableProfile">
+						<thead>
+							<tr>
+								<th>Consommation électrique</th>
+								<th>Consommation de chauffage</th>
+								<th>Économie</th>
+							</tr>
+					  </thead>
+						<tbody>
+							<tr>
+								<td>100W</td>
+								<td>200W</td>
+								<td>30W</td>
+							</tr>
+							<tr>
+								<td>100W</td>
+								<td>200W</td>
+								<td>30W</td>
+							</tr>
+							<tr>
+								<td>100W</td>
+								<td>200W</td>
+								<td>30W</td>
+							</tr>
+							<tr>
+								<td>100W</td>
+								<td>200W</td>
+								<td>30W</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div style="flex:1">
+						<canvas id="canvas" width="800" height="500"></canvas>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="rectangleContenu" id="rectangleContenu3">
+			<div class="title">
+				Contrôler mes actionneurs
+			</div>
+		</div>
+		<script type="text/javascript" src="Vues/javascript/client.js"></script>
+	</body>
+	</html>
+
+
+<?php include('Vues/gestion_piece.php');?>
---
+++ Vues/javascript/client.js
@@ -0,0 +1,150 @@
+let step = 1;
+changeStep(1); // pour se mettre à l'étape 1 dès le départ
+
+// fonction pour changer l'étape
+function changeStep(s){
+	step = s;
+
+	// on commence par change le background du btn séléctionné
+	for(let i=1; i<=3; i++){
+		if(i==s){
+			document.getElementById(i).classList.add("btn-menu-selected");
+			document.getElementById("rectangleContenu"+i).style.display = "block"
+		}else{
+			// on enlève la class selected des autres boutons
+			document.getElementById(i).classList.remove("btn-menu-selected");
+			document.getElementById("rectangleContenu"+i).style.display = "none"
+
+		}
+	}
+}
+
+function ajouterHabitat(){
+
+	document.getElementById("newHabitat").style.display = "block"
+
+}
+
+
+function ajouterPiece(){
+
+	document.getElementById("newCapteur").style.display = "block"
+
+}
+
+function listePiece(){
+
+	document.getElementById("listePiece").style.display = "block"
+
+}
+
+
+// gestion du modal
+
+function openModal(e){
+	document.getElementById('wrapper').style.display="block";
+}
+
+function closeModal(e){
+	document.getElementById('wrapper').style.display="none";
+}
+const stopPropagation=function(e){
+    // empêche la propagation du click aux parents > empeche de fermer quand on clique dans la fenetre
+    e.stopPropagation(); //
+}
+
+$('#modal-close').click(function(e) {
+	closeModal(e);
+});
+
+$('.btn-piece').click(function(e) {
+	openModal(e);
+});
+
+
+
+window.addEventListener('keydown',function(e){
+    if(e.key == "Escape" || e.key=="Esc"){
+        closeModal(e);
+    }
+})
+
+
+
+
+// graphique
+
+
+var drawPieChart = function(data, colors) {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var x = canvas.width / 2;
+      y = canvas.height / 2;
+  var color,
+      startAngle,
+      endAngle,
+      total = getTotal(data);
+
+  for(var i=0; i<data.length; i++) {
+    color = colors[i];
+    startAngle = calculateStart(data, i, total);
+    endAngle = calculateEnd(data, i, total);
+
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, y-100, startAngle, endAngle);
+    ctx.fill();
+    ctx.rect(canvas.width - 200, y - i * 30, 12, 12);
+    ctx.fill();
+    ctx.font = "13px sans-serif";
+    ctx.fillText(data[i].label, canvas.width - 200 + 20, y - i * 30 + 10);
+  }
+};
+
+var calculatePercent = function(value, total) {
+
+  return (value / total * 100).toFixed(2);
+};
+
+var getTotal = function(data) {
+  var sum = 0;
+  for(var i=0; i<data.length; i++) {
+    sum += data[i].value;
+  }
+
+  return sum;
+};
+
+var calculateStart = function(data, index, total) {
+  if(index === 0) {
+    return 0;
+  }
+
+  return calculateEnd(data, index-1, total);
+};
+
+var calculateEndAngle = function(data, index, total) {
+  var angle = data[index].value / total * 360;
+  var inc = ( index === 0 ) ? 0 : calculateEndAngle(data, index-1, total);
+
+  return ( angle + inc );
+};
+
+var calculateEnd = function(data, index, total) {
+
+  return degreeToRadians(calculateEndAngle(data, index, total));
+};
+
+var degreeToRadians = function(angle) {
+  return angle * Math.PI / 180
+}
+
+var data = [
+  { label: 'Consommation électrique', value: 130 },
+  { label: 'Consommation de chauffage', value: 150 },
+  { label: 'Economies', value: 80 }
+];
+var colors = [ '#f86920', '#30d99c', '#32ace1', '#e9e0d7' ];
+
+drawPieChart(data, colors);
